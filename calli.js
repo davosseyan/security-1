a758b1e664f274c68a1415a5b41d473ca0b4b7ab
hede seyan <78200081+davosseyan@users.noreply.github.com>
2021-03-23 12:30:36 +0300

    Update calli.js

--- calli.js
+++ calli.js
@@ -96,7 +96,7 @@ calli.on("message", async message => {
 \`${prefix}anti channelC\`: **Number**
 \`${prefix}anti roleD\`: **Number**
 \`${prefix}anti roleC\`: **Number**
-\`${prefix}anti time **Number**
+\`${prefix}anti time\`: **Number**
 **Security On/Off**
 \`${prefix}anti bot\`: **on-off**
 **Security**
@@ -1167,3 +1167,28 @@ fs.writeFile("./antibots.json", JSON.stringify(antibots), err => {
     });
 });
 ///////////////////////////////////////////////////////////////////////////////
+client.on("message", message => {
+  let commands = message.content.split(" ");
+  if (commands[0] == prefix + "say") {
+    if (!message.guild) return;
+    if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES"))
+      return message.reply("**You Dont Have `MANAGE_MESSAGES` Permission .**");
+    if (!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES"))
+      return message.reply(
+        "Please Check My Role Permission To `MANAGE_MESSAGES`"
+      );
+    var args = message.content
+      .split(" ")
+      .slice(1)
+      .join(" ");
+    if (!args) {
+      return message.channel.send("`Usage : " + prefix + "say <message>`");
+    }
+    message.delete();
+    var embed = new Discord.MessageEmbed()
+      .setColor("#000000")
+      .setDescription(`${args}`)
+      .setFooter(`By ${message.author.tag}`);
+    message.channel.send(embed);
+  }
+})
