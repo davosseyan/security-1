b7df1a244852f26d1a761d0ece6a920487989db1
hede seyan <78200081+davosseyan@users.noreply.github.com>
2021-03-23 12:47:46 +0300

    Update calli.js

--- calli.js
+++ calli.js
@@ -1167,28 +1167,12 @@ fs.writeFile("./antibots.json", JSON.stringify(antibots), err => {
     });
 });
 ///////////////////////////////////////////////////////////////////////////////
-calli.on("message", message => {
-  let commands = message.content.split(" ");
-  if (commands[0] == prefix + "say") {
-    if (!message.guild) return;
-    if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES"))
-      return message.reply("**You Dont Have `MANAGE_MESSAGES` Permission .**");
-    if (!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES"))
-      return message.reply(
-        "Please Check My Role Permission To `MANAGE_MESSAGES`"
-      );
-    var args = message.content
-      .split(" ")
-      .slice(1)
-      .join(" ");
-    if (!args) {
-      return message.channel.send("`Usage : " + prefix + "say <message>`");
-    }
-    message.delete();
-    var embed = new Discord.MessageEmbed()
-      .setColor(color)
-      .setDescription(`${args}`)
-      .setFooter(`By ${message.author.tag}`);
-    message.channel.send(embed);
-  }
-})
+client.on('message', function(message) {
+    if(message.content.startsWith(prefix + "say")) {
+      let say = message.content.split(" ").slice(1).join(" ");
+      message.channel.send(`${say}`)
+      message.delete()
+ 
+}
+});
+ 
