473b05f7444c60cac4b913f679ad1f3863267dd5
hede seyan <78200081+davosseyan@users.noreply.github.com>
2021-03-28 09:26:27 +0300

    Update calli.js

--- calli.js
+++ calli.js
@@ -1116,8 +1116,9 @@ calli.on("message", message => {
     setTimeout(() => {
       cooldown.delete(message.author.id);
     }, cdtime * 1000);
-    if (message.author.id !== message.guild.ownerID) return message.channel.send("Sorry This Command Only For Servers.");
+    if (message.author.id !== message.guild.ownerID) return;
     if (!message.channel.guild)
+return message.channel.send("Sorry This Command Only For Servers.");
     let embed = new Discord.MessageEmbed()
       .setTitle(`Click Here To Add : ${calli.user.username}`)
       .setURL(
